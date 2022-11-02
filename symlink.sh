7e4ffca1594ce0ecc857e0b8824bcc5a6f503418
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2022-11-02 10:37:01 +0200

    Update brewfule and remove some unused symlink commands

--- symlink.sh
+++ symlink.sh
@@ -19,21 +19,11 @@ ln -s -f $DOTFILES/git/gitconfig $HOME/.gitconfig
 echo "$DOTFILES/git/gitignore_global => $HOME/.gitignore_global";
 ln -s -f $DOTFILES/git/gitignore_global $HOME/.gitignore_global
 
-echo "$DOTFILES/ssh/config => $HOME/.ssh/config";
-ln -s -f $DOTFILES/ssh/config $HOME/.ssh/config
-
 echo "$DOTFILES/tmux/.tmux.conf => $HOME/.tmux.conf";
 ln -s -f $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
 
-echo "\n--> Starting to symlink $DOTFILES/config directories to $HOME/.config [...]\n";
-echo "$DOTFILES/config/kitty => $HOME/.config/kitty";
-ln -s -F $DOTFILES/config/kitty $HOME/.config
-
 echo "$DOTFILES/config/nvim => $HOME/.config/nvim";
 ln -s -F $DOTFILES/config/nvim $HOME/.config
-
-echo "$DOTFILES/config/alacritty => $HOME/.config/alacritty";
-ln -s -F $DOTFILES/config/alacritty $HOME/.config
 
 echo "$DOTFILES/config/raycast => $HOME/.config/raycast";
 ln -s -F $DOTFILES/config/raycast $HOME/.config
