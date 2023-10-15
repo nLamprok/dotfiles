f1f5593a0ee4b2ac356d0397f8192c929c43d2e0
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2023-10-15 16:05:04 +0300

    feat(wezterm): add simple configuration for wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.enable_tab_bar = false
+config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
+config.font_size = 14.0
+config.color_scheme = 'Rosé Pine (Gogh)'
+config.window_decorations = "RESIZE"
+
+return config
