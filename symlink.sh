334d70d19009331ad80ec9bf3b2990e058b00a9b
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2024-10-20 13:32:32 +0300

    fix: finally move tmux to config and small revamp

--- symlink.sh
+++ symlink.sh
@@ -2,34 +2,37 @@ DOTFILES=$(pwd)
 
 while true; do
   read -p "Symlinking will override existing files. Are you sure? (y/N) " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
+  case $yn in
+  [Yy]*) break ;;
+  [Nn]*) exit ;;
+  *) echo "Please answer yes or no." ;;
+  esac
 done
 
-echo "\n--> Starting root symlinks: \n";
+echo "\n--> Starting root symlinks: \n"
 
-echo "$DOTFILES/zsh/.zshrc => $HOME/.zshrc";
+echo "$DOTFILES/zsh/.zshrc => $HOME/.zshrc"
 ln -s -f $DOTFILES/zsh/.zshrc $HOME/.zshrc
 
-echo "$DOTFILES/git/gitconfig => $HOME/.gitconfig";
+echo "$DOTFILES/git/gitconfig => $HOME/.gitconfig"
 ln -s -f $DOTFILES/git/gitconfig $HOME/.gitconfig
 
-echo "$DOTFILES/git/gitignore_global => $HOME/.gitignore_global";
+echo "$DOTFILES/git/gitignore_global => $HOME/.gitignore_global"
 ln -s -f $DOTFILES/git/gitignore_global $HOME/.gitignore_global
 
-echo "$DOTFILES/tmux/.tmux.conf => $HOME/.tmux.conf";
-ln -s -f $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
+echo "$DOTFILES/config/tmux => $HOME/.config/tmux"
+ln -s -F $DOTFILES/config/tmux $HOME/.config
 
-echo "$DOTFILES/config/nvim => $HOME/.config/nvim";
+echo "$DOTFILES/config/nvim => $HOME/.config/nvim"
 ln -s -F $DOTFILES/config/nvim $HOME/.config
 
-echo "$DOTFILES/config/zed/keymap.json => $HOME/.config/zed/keymap.json";
+echo "$DOTFILES/config/kitty => $HOME/.config/kitty"
+ln -s -F $DOTFILES/config/kitty $HOME/.config
+
+echo "$DOTFILES/config/zed/keymap.json => $HOME/.config/zed/keymap.json"
 ln -s -F $DOTFILES/config/zed/keymap.json $HOME/.config/zed/keymap.json
 
-echo "$DOTFILES/config/zed/settings.json => $HOME/.config/zed/settings.json";
+echo "$DOTFILES/config/zed/settings.json => $HOME/.config/zed/settings.json"
 ln -s -F $DOTFILES/config/zed/settings.json $HOME/.config/zed/settings.json
 
 echo "\nDone :)"
